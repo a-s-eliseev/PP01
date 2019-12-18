d1b7eb60451a6df31be22d1bd3a830981d9d0e93
a-s-eliseev <a.s.eliseev@bk.ru>
2019-12-18 17:22:22 +0400

    v02.1 fixed file path

--- src/main/java/app/DAO/UserDAO.java
+++ src/main/java/app/DAO/UserDAO.java
@@ -2,11 +2,6 @@ package app.DAO;
 
 import app.entities.User;
 
-import javax.servlet.RequestDispatcher;
-import javax.servlet.ServletException;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
 import java.sql.*;
 import java.util.ArrayList;
 import java.util.List;
--- src/main/java/app/util/PropertyReader.java
+++ src/main/java/app/util/PropertyReader.java
@@ -1,14 +1,16 @@
 package app.util;
 
 import java.io.*;
+import java.util.Objects;
 import java.util.Properties;
 
 public class PropertyReader {
 
     public static String getProperty(String nameParam) throws IOException {
         Properties properties = new Properties();
-        File file = new File("C:\\IdeaProjects\\PP01\\src\\main\\resources\\properties");
-        properties.load(new FileReader(file));
+        File fileProperties = new File(Objects.requireNonNull(PropertyReader.class.getClassLoader().getResource("properties")).getFile());
+        //File file = new File("C:\\IdeaProjects\\PP01\\src\\main\\resources\\properties");
+        properties.load(new FileReader(fileProperties));
         return properties.getProperty(nameParam);
     }
 }
